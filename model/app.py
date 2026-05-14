cd4ccdc09aa7eb7b2c8d9db0fe7a76cf318987fb
sohanbarat-dev <sohanbarat2003@gmail.com>
2026-05-14 15:45:45 +0530

    Add Dockerfile for HuggingFace Spaces

--- model/app.py
+++ model/app.py
@@ -250,6 +250,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5001))
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 7860))
     app.run(host='0.0.0.0', port=port, debug=False)
